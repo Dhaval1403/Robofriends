10060743ced09f5f94d5ab859e203a45b8fb6163
Dhaval Mehta <mehtadhaval8228@gmail.com>
2020-01-10 17:54:31 +0530

    Updates

---
+++ precache-manifest.d5f549c52f092df32b3b9bdd2d1a9fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbece8bb75e44d84149bb8629b1e13eb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2cfffb01a4b1f2c2d1d0",
+    "url": "/robofriends/static/css/main.d57af491.chunk.css"
+  },
+  {
+    "revision": "25516ca3bfbf1e945c42",
+    "url": "/robofriends/static/js/2.13d104f2.chunk.js"
+  },
+  {
+    "revision": "1977d94acb8517e64079a472548bc889",
+    "url": "/robofriends/static/js/2.13d104f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cfffb01a4b1f2c2d1d0",
+    "url": "/robofriends/static/js/main.35497288.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
